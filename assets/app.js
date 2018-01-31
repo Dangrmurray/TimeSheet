a7dcdeef819788a4c2981a718fb838de991d90ee
Daniel Murray <danielmurray@Daniels-MacBook-Pro.local>
2018-01-31 13:03:43 -0800

    varconfig

---
+++ assets/app.js
@@ -0,0 +1,9 @@
+ var config = {
+    apiKey: "AIzaSyA8WNiAKXApRoXk48iDn8hGGEF_LXSPeQY",
+    authDomain: "timesheet-24b7c.firebaseapp.com",
+    databaseURL: "https://timesheet-24b7c.firebaseio.com",
+    projectId: "timesheet-24b7c",
+    storageBucket: "timesheet-24b7c.appspot.com",
+    messagingSenderId: "769174533846"
+  };
+  firebase.initializeApp(config);
